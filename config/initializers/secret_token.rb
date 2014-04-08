71c04bdb6488151ead1d8f34240fc07d8d6f5da1
Brad Lowton <bradlowton@hotmail.com>
2014-04-08 05:46:57 -0700

    First repository done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Groom::Application.config.secret_key_base = 'c9d06f9521c47b2b4826f1e9b47d6bfe49e255a567dd3eb1e3524e826aa91fde6265b06f80fbddd5f8ab702a9bf3e9f238c9fdab3ea21eb952cae1037c647ef2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Groom::Application.config.session_store :cookie_store, key: '_groom_session'
